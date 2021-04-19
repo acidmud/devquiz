ef454b1f83a53b41569bedef6028a4fb91a6f90e
renamcomn <renan.ferreira@coachingtech.com.br>
2021-04-19 17:44:08 -0300

    Adicionado Splash Fade Transition

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,7 +1,32 @@
 import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    Future.delayed(Duration(seconds: 4)).then((_) {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (c, a1, a2) => HomePage(),
+          transitionsBuilder: (c, anim, a2, child) =>
+              FadeTransition(opacity: anim, child: child),
+          transitionDuration: Duration(milliseconds: 1000),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
