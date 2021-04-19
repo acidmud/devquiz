a638923f92d329a43c9a8afe0fe5218b7532e7d1
renamcomn <renan.ferreira@coachingtech.com.br>
2021-04-19 15:22:05 -0300

    AppBar - AULA01

---
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -0,0 +1,48 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/score_card/score_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends PreferredSize {
+  AppBarWidget()
+      : super(
+            preferredSize: Size.fromHeight(250),
+            child: Container(
+              height: 250,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    height: 161,
+                    width: double.maxFinite,
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(TextSpan(
+                            text: "Olá ",
+                            style: AppTextStyles.title,
+                            children: [
+                              TextSpan(
+                                  text: "Renan Henrique",
+                                  style: AppTextStyles.titleBold)
+                            ])),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 20),
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              image: DecorationImage(
+                                  image: NetworkImage(
+                                      "https://avatars.githubusercontent.com/u/26748562?v=4"))),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Align(
+                      alignment: Alignment(0.0, 1.0), child: ScoreCardWidget())
+                ],
+              ),
+            ));
+}
---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,53 @@
+import 'package:DevQuiz/core/core.dart';
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: AppColors.purple.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(flex: 1, child: ChartWidget()),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Vamos começar",
+                      style: AppTextStyles.heading,
+                    ),
+                    Text(
+                      "Complete os desafios e avance em conhecimento",
+                      style: AppTextStyles.body,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
